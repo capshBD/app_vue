31fd6877943a628ba31e657c60df910df225549d
unknown <capshBD@yeah.net>
2019-11-30 17:14:43 +0800

    1130

--- src/main/java/com/sunway/vue/controller/VueStudyController.java
+++ src/main/java/com/sunway/vue/controller/VueStudyController.java
@@ -1,8 +1,14 @@
 package com.sunway.vue.controller;
 
+import com.sunway.vue.domain.VueDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @program: app_vue
@@ -124,5 +130,29 @@ public class VueStudyController {
     @GetMapping("/comp-ref-example")//父子引用例子
     public String testRefExample(){
         return "vue/html/vueRefExample";
+    }
+
+    @GetMapping("/router")//路由
+    public String testRouter(){
+        return "vue/html/vueRouter";
+    }
+
+    @GetMapping("/resource")//ajax
+    public String testResouce(){
+        return "vue/html/vueResource";
+    }
+
+    @GetMapping("/getVueData")//ajax
+    @ResponseBody
+    public List<VueDTO>  testAjax(){
+      List<VueDTO> list=new ArrayList<>();
+      for (int i=0;i<10;i++){
+          VueDTO vue=new VueDTO();
+          vue.setId(UUID.randomUUID().toString());
+          vue.setVueName("小明"+i);
+          vue.setVueStrange("奇诡");
+          list.add(vue);
+      }
+     return list;
     }
 }
